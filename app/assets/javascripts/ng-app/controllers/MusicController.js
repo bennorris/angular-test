b581033a9c41a0812451d865c4a874932b465206
github username <github email address>
2016-12-14 20:07:18 -0500

    music section now drills down an additional level by making new request to last.fm album details

--- app/assets/javascripts/ng-app/controllers/MusicController.js
+++ app/assets/javascripts/ng-app/controllers/MusicController.js
@@ -1,15 +1,41 @@
 function MusicController(Album, $scope) {
   var ctrl = this;
+  $scope.listeners = [];
+  $scope.items = [];
+  $scope.full = [];
+  $scope.style = "none";
 
 $scope.searchRecord = function () {
+  $scope.items = [];
   Album
     .getAlbum($scope.search)
     .then(function(result) {
       ctrl.data = result.data;
+      $scope.full = Album.getDetailedAlbum(result.data);
       $scope.search = '';
+      console.log($scope.full);
     })
-
 }
+
+$scope.showDetails = function() {
+  $scope.style = "inline"
+}
+
+// $scope.getAlbumInfo = function() {
+//   $scope.recordInfoz = [];
+//   var slamDunk = ctrl.data.topalbums.album;
+//   slamDunk.sort(function(a,b) {
+//     return parseFloat(b.playcount) - parseFloat(a.playcount)
+//   })
+//
+//   for (var i = 0; i < 3; i++) {
+//     Album.getDetailedAlbum(slamDunk[i].artist.name, slamDunk[i].name)
+//     .then(function(res) {
+//       $scope.description.push(res.data.album.wiki.content);
+//     })
+//   }
+// }
+
 
 }
 
--- app/assets/javascripts/ng-app/services/Album.js
+++ app/assets/javascripts/ng-app/services/Album.js
@@ -6,8 +6,23 @@ this.getAlbum = function(artist) {
   return $http.get('http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=' + artist + '&api_key=' + lastFmKey + '&format=json')
  }
 
+this.getDetailedAlbum = function(arr) {
+  var full = [];
+  var slamDunk = arr.topalbums.album;
+  slamDunk.sort(function(a,b) {
+    return parseFloat(b.playcount) - parseFloat(a.playcount)
+  })
+
+  for (var i = 0; i < 3; i++) {
+    $http.get('http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=' + lastFmKey + '&artist=' + slamDunk[i].artist.name + '&album=' + slamDunk[i].name + '&format=json')
+    .then(function(res) {
+      full.push(res)
+    })
+ }
+ return full;
 }
 
+}
 angular
   .module('app')
   .service('Album', Album);
