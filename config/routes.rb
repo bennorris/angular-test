b94eeae0fa75b663b09c398e958b4baa81bfad6e
github username <github email address>
2016-12-16 14:26:57 -0500

    updated routes and db

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root 'application#index'
-    get '*path' => 'application#index'
+  get '/api/users' => 'users#index'
+  post '/api/users' => 'users#create'
+  get '*path' => 'application#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161216134031) do
+ActiveRecord::Schema.define(version: 20161216154215) do
 
   create_table "users", force: :cascade do |t|
-    t.string  "name"
-    t.integer "facebook"
+    t.string "name"
+    t.string "facebook"
   end
 
 end
